276d7c17506b1f9ab0069c80b2f792a3e3e5152b
olivers2028-dot <olivers2028@student.cis.edu.hk>
2025-09-10 12:26:40 +0200

    Dateable.java: Completed task requirment

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,24 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to Dateable Calculator");   // Prints welcome banner
+
+        Scanner scanner = new Scanner(System.in);               // Start scanner
+
+        System.out.println("Enter your age");                   // Asks for user age input
+
+        int age = -1;                                           // Input validation
+        while (age < 0){
+            try{
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e){
+                System.out.println("Invalid input. Please enter a valid age");
+            }
+        }
+        System.out.println("The maximum age you can date is: " + age/2 + 7);    // Output
+
+
     }
 }
