5e98f7f0ac23566304643556cb0f601c57489b70
olivers2028-dot <olivers2028@student.cis.edu.hk>
2025-09-10 12:38:22 +0200

    Pages.java: Completed according to task requirements

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,23 @@
-public class Pages {
+import java.util.Scanner;
 
+public class Pages {
     public static void main(String[] args) {
+        System.out.println("Welcome to \"Pages before Giving Up\" Calculator!");   // Prints welcome banner
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");                   // Asks for user age input
+
+        int age = -1;                                           // Input validation
+        while (age < 0){
+            try{
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e){
+                System.out.println("Invalid input. Please enter a valid age");
+            }
+        }
+        System.out.println("Pages you, a " + age + "-year-old, should read before giving up: " + (100-age));    // Output
+
 
     }
 }
